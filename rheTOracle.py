c819fd55b19beb8e552946003f75a9c48f748629
Sean Azlin <sazlin@gmail.com>
2014-06-20 15:46:23 -0700

    Created a basic Flask app for us to start with.

---
+++ rheTOracle.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return say_hello()
+
+def say_hello():
+    return u'RheTOracly speaking, how awesome are we?'
+
+if __name__ == '__main__':
+    app.run()
