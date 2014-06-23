22265388785ca532c2d8f2fdee63e67ce5ca3441
Sean Azlin <sazlin@gmail.com>
2014-06-23 14:21:10 -0700

    parameterized drawVisualization() and added drawTestVisualization() for the play_visualiztion.html file. Also modified that html file to use drawTestVisualization().

--- static/drawVisualization.js
+++ static/drawVisualization.js
@@ -1,4 +1,4 @@
-function drawVisualization() {
+function drawTestVisualization(){
   // Create and populate the data table.
   var data = google.visualization.arrayToDataTable([
     ['@User', '#Seattle'],
@@ -8,7 +8,10 @@ function drawVisualization() {
     ['JohnShivero', 83],
     ['Muazify911', 65]
   ]);
+  drawVisualization(data, 'visualization');
+}
 
+function drawVisualization(data, target) {
   // Create and draw the visualization.
   var view = new google.visualization.DataView(data);
   view.setColumns([0, 1,
@@ -21,6 +24,6 @@ function drawVisualization() {
                  hAxis: {gridlines: {count: 0}, baselineColor: 'none'},
                  legend: { position: "none" }
                 };
-  var chart = new google.visualization.BarChart(document.getElementById('visualization'));
+  var chart = new google.visualization.BarChart(document.getElementById(target));
   chart.draw(view, options);
 }
