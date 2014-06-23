6781da85a4fc174ecce7e1426f6d2b8bdb7ae6e4
Sean Azlin <sazlin@gmail.com>
2014-06-23 16:13:26 -0700

    Fleshed out home.html to have 4 graphs per our wireframes. Added 4 views for the 4 related queries that don't do anything except redirect at the moment. Added a bit of CSS so that the graphs look right.

--- rheTOracle.py
+++ rheTOracle.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from flask import render_template
 app = Flask(__name__)
 
@@ -21,6 +21,30 @@ def contact_page():
 @app.route('/test-graph', methods=['GET'])
 def test_graph():
     return render_template('test_graphs.html')
+
+
+@app.route('/q1', methods=['GET'])
+def q1_query(q1_what="#Seattle"):
+    """Who is talking about #q1_what?"""
+    return redirect(url_for('home_page'))
+
+
+@app.route('/q2', methods=['GET'])
+def q2_query(q2_who="@crisewing"):
+    """What is @q2_who talking about?"""
+    return redirect(url_for('home_page'))
+
+
+@app.route('/q3', methods=['GET'])
+def q3_query(q3_who="@CodeFellowsOrg"):
+    """Who is talking about @q3_who?"""
+    return redirect(url_for('home_page'))
+
+
+@app.route('/q4', methods=['GET'])
+def q4_query(q4_who="#Seattle"):
+    """Who is @q4_who talking about?"""
+    return redirect(url_for('home_page'))
 
 if __name__ == '__main__':
     app.run()
