abf1df7b96234938056ee5066f9eba24abb22b61
miramzz <mira.muazzez@gmail.com>
2014-06-23 16:13:28 -0700

    base commit

--- twitter_draft.py
+++ twitter_draft.py
@@ -2,17 +2,26 @@ import tweepy
 import header_rheTOracle as h
 
 call_back_url = 'ec2-54-213-173-105.us-west-2.compute.amazonaws.com'
-# auth setup
-auth = tweepy.OAuthHandler(h.twit_consumer_key, h.twit_consumer_secret, call_back_url)
-auth.set_access_token(h.twit_access_token, h.twit_access_token_secret)
-
-# gets the tweets
-api = tweepy.API(auth)
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print tweet.text
 
 
-# sends a tweet
-api = tweepy.API(auth)
-api.update_status('tweepy + oadads')
+def make_conn():
+    auth = tweepy.OAuthHandler(h.twit_consumer_key, h.twit_consumer_secret, call_back_url)
+    auth.set_access_token(h.twit_access_token, h.twit_access_token_secret)
+    try:
+        redirect_url = auth.get_authorization_url()
+    except tweepy.TweepError:
+        print 'Error! Failed to get request token.'
+    return redirect_url, auth
+
+
+def search(query, auth):
+    # max_tweets = 250
+    api = tweepy.API(auth)
+    f = open('queryoutput4.html', 'w+')
+    for tweet in tweepy.Cursor(api.search, q=query).items():
+        f.write(str(tweet) + '\n')
+
+
+if __name__ == '__main__':
+    redirect_url, auth = make_conn()
+    search("obama", auth)
