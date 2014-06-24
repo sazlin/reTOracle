1009117a55707331af25e5fecbda9ccd4bcfcae5
Sean Azlin <sazlin@gmail.com>
2014-06-24 15:46:07 -0700

    Few small changes to sql scripts, including to make urls a text[] instead of a text column. Added sql connection code to rhetoracle.py so that it returns json results for /q1.

--- rheTOracle.py
+++ rheTOracle.py
@@ -1,6 +1,90 @@
 from flask import Flask, redirect, url_for
 from flask import render_template
+import psycopg2
+import json
+#from passlib.hash import pbkdf2_sha256
+from secrets import SECRETS
 app = Flask(__name__)
+
+app.config['DB_HOST'] = "rhetoracle-db-instance.c2vrlkt9v1tp.us-west-2.rds.amazonaws.com"
+app.config['DB_NAME'] = "rhetorical-db"
+app.config['DB_USERNAME'] = SECRETS['DB_USERNAME']
+#app.config['DB_PASSWORD'] = pbkdf2_sha256.encrypt(SECRETS['DB_PASSWORD'])
+app.config['DB_PASSWORD'] = SECRETS['DB_PASSWORD']
+app.config['SECRET_KEY'] = SECRETS['FLASK_SECRET_KEY']
+app.config['DB_CONNECTION'] = None
+app.config['DB_CURSOR'] = None
+
+
+
+def build_connection_string():
+        connection_string = []
+        connection_string.append("host=" + app.config['DB_HOST'])
+        connection_string.append("dbname=" + app.config['DB_NAME'])
+        connection_string.append("user=" + app.config['DB_USERNAME'])
+        connection_string.append("password=" + app.config['DB_PASSWORD'])
+        return " ".join(connection_string)
+
+
+def connect_db():
+    """establish a new connection"""
+    connection_string = build_connection_string()
+    try:
+        print "establishing a new connection..."
+        conn = psycopg2.connect(connection_string)
+    except Exception as x:
+        print "Error connecting to DB: ", x.args
+    print "Connection established and stored..."
+    app.config['DB_CONNECTION'] = conn
+    return conn
+
+
+def get_connection():
+    """get the current connection if it exists, else connect."""
+    if app.config['DB_CONNECTION'] is not None:
+        print "connection exists, so reusing it..."
+        return app.config['DB_CONNECTION']
+    else:
+        print "no connection found..."
+        return connect_db()
+
+
+def create_cursor():
+    """create a new cursor and store it"""
+    conn = get_connection()
+    app.config['DB_CURSOR'] = conn.cursor()
+    return app.config['DB_CURSOR']
+
+
+def get_cursor():
+    """get the current cursor if it exist, else create a new cursor"""
+    if app.config['DB_CURSOR'] is not None:
+        print "cursor exists, using that..."
+        return app.config['DB_CURSOR']
+    else:
+        print "no cursor found, so creating one..."
+        return create_cursor()
+
+
+def execute_query(sql):
+    """execute the passed in SQL using the current cursor"""
+    print "executing query..."
+    try:
+        cur = get_cursor()
+    except Exception as x:
+        print "Something went wrong: ", x.args
+    print "got cursor..."
+    cur.execute(sql)
+    try:
+        results = cur.fetchall()
+        print "Raw Results: ", results
+    except Exception as x:
+        print "Error executing sql on cursor: ", x.args
+    else:
+        print "query success!"
+        print "Returning: ", json.dumps(results)
+        return json.dumps(results)
+    return None
 
 
 @app.route('/', methods=['GET'])
@@ -26,7 +110,14 @@ def test_graph():
 @app.route('/q1', methods=['GET'])
 def q1_query(q1_what="#Seattle"):
     """Who is talking about #q1_what?"""
-    return redirect(url_for('home_page'))
+    sql = """
+    SELECT screen_name, COUNT(screen_name) as TweetCount
+    FROM massive
+    WHERE '#Seattle' = ANY (hashtags)
+    GROUP BY screen_name
+    """
+    json_result = execute_query(sql)
+    return json_result
 
 
 @app.route('/q2', methods=['GET'])
