1792bb9644c2928921173f5b6bfb9a6e1cbf3c84
Sean Azlin <sazlin@gmail.com>
2014-06-24 11:09:16 -0700

    added two sql scripts. One drops and recreates the massive table from rhetorical-db, which we'll want to use for deployment eventually. The second script is an example insertion that can inform how we want to build our twitter stream worker. The example insertion can also be used to help test our db eventually.

---
+++ static/sql/example_db_insertion.sql
@@ -0,0 +1,22 @@
+﻿INSERT INTO massive (
+	tweet_id, 
+	text, 
+	hashtags, 
+	user_mentions, 
+	created_at, 
+	screen_name, 
+	url, 
+	location, 
+	inreplytostatusif, 
+	retweetcount)
+VALUES (
+	'testrecordid12345',
+	'This is a fake tweet inserted into the db manually',
+	'{#Seattle,#Python}',
+	'{@crisewing,@SeanAzlin2}',
+	now(),
+	'@CodeFellowsOrg',
+	'https://twitter.com/SeanAzlin2/status/481243614211620866',
+	'["47.614848","-122.3358423"]',
+	'False',
+	0);
