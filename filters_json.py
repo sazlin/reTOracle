3c0d4c89e2ea993b4a65bf38f13d2be2b730f1a5
John Shiver <johneshiver@gmail.com>
2014-06-25 14:20:49 -0700

    Convert stream filters to JSON

---
+++ filters_json.py
@@ -0,0 +1,82 @@
+import json
+
+filter_list = {"c": {'search_terms':
+                       {'hashtags': ['#cprogramming', '#clanguage'],
+                        'users': [],
+                        'keywords': []},
+                     'blacklist':
+                       {'hashtags': [],
+                        'users': [],
+                        'keywords': []}},
+"python": {'search_terms':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []},
+           'blacklist':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []}},
+"java": {'search_terms':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []},
+           'blacklist':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []}},
+"javascript": {'search_terms':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []},
+           'blacklist':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []}},
+"ruby": {'search_terms':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []},
+           'blacklist':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []}},
+"php": {'search_terms':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []},
+           'blacklist':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []}},
+"csharpsharp": {'search_terms':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []},
+           'blacklist':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []}},
+"objectivec": {'search_terms':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []},
+           'blacklist':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []}},
+"swift": {'search_terms':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []},
+           'blacklist':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []}},
+"python": {'search_terms':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []},
+           'blacklist':
+                {'hashtags': ['#python', '#django', '#pyconau', '#numpy'],
+                 'users': ['gvanrossum'],
+                 'keywords': []}}}
