b4db33494ce1530c9a2311654c13b2a042ff373e
Sean Azlin <sazlin@gmail.com>
2014-06-26 11:27:20 -0700

    Now generating sql query string for Q2 dynamically using filter_list, though w/o mapping the sql results.

--- rheTOracle.py
+++ rheTOracle.py
@@ -129,9 +129,75 @@ def build_q1_querystring():
     sql.append("""ORDER BY HashTagCount DESC""")
     return " \r\n".join(sql)
 
+def build_q2_querystring():
+    """build a query string for Q2 using filter_list"""
+    sql = []
+    sql.append("""SELECT hashtag, HashTagCount, screen_name""")
+    sql.append("""FROM (SELECT hashtag, screen_name, HashTagCount, rank() OVER (PARTITION BY hashtag ORDER BY HashTagCount DESC, screen_name) AS pos""")
+    sql.append("""FROM (SELECT hashtag, screen_name, COUNT(hashtag) as HashTagCount""")
+    sql.append("""FROM (SELECT screen_name, unnest(hashtags) as hashtag FROM massive) as unwrap""")
+    sql.append("""WHERE""")
+    for language in filter_list:
+        search_terms = filter_list[language]['search_terms']
+        for hashtag in search_terms['hashtags']:
+            sql.append("""hashtag = '""" + hashtag[1:] + """'""")
+            sql.append("""OR""")
+        # for user in search_terms['users']:
+        #     sql.append("""screen_name = '""" + user + """'""")
+        #     sql.append(""" OR """)
+        # for keyword in search_terms['keywords']:
+        #     sql.append("""'"""+ keyword + """' LIKE text""")
+        #     sql.append(""" OR """)
+    sql.pop()  # discard last OR statement
+    sql.append("""GROUP BY screen_name, hashtag""")
+    sql.append("""ORDER BY hashtag, HashTagCount DESC""")
+    sql.append(""") as countedhashtags""")
+    sql.append(""") as ss""")
+    sql.append("""WHERE pos = 1""")
+    sql.append("""ORDER BY hashtag, HashTagCount DESC""")
+    return " \r\n".join(sql)
+    """
+    SELECT hashtag, HashTagCount, screen_name
+    FROM
+    (
+        SELECT hashtag, screen_name, HashTagCount, rank() OVER (PARTITION BY hashtag ORDER BY HashTagCount DESC, screen_name) AS pos
+        FROM (
+            SELECT hashtag, screen_name, COUNT(hashtag) as HashTagCount
+            FROM (
+                SELECT screen_name, unnest(hashtags) as hashtag
+                FROM massive
+            ) as unwrap
+            WHERE
+            hashtag = 'Java' OR
+            hashtag = 'Python' OR
+            hashtag = 'JavaScript' OR
+            hashtag = 'CPlusPlus' OR
+            hashtag = 'Java'
+            GROUP BY screen_name, hashtag
+            ORDER BY hashtag, HashTagCount DESC
+        ) as countedhashtags
+    ) as ss
+    WHERE pos = 1
+    ORDER BY hashtag, HashTagCount DESC
+    """
 
 def map_q1_results_to_language(parsed_results):
     """use the filter_list to group and sum the results parsed from Q1's query results
+        into a new list of lists that will be returned to the client for rendering"""
+    final_result = []
+    for language in filter_list:
+        language_count = 0
+        search_terms = filter_list[language]['search_terms']
+        for hashtag in search_terms['hashtags']:
+            for result in parsed_results:
+                print "***comparing***", hashtag[1:], result[0]
+                if hashtag[1:] == result[0]:
+                    language_count += result[1]
+        final_result.append([language, language_count])
+    return json.dumps(final_result)
+
+def map_q2_results_to_language(parsed_results):
+    """use the filter_list to group and sum the results parsed from Q2's query results
         into a new list of lists that will be returned to the client for rendering"""
     final_result = []
     for language in filter_list:
@@ -202,8 +268,12 @@ def q2_query():
     WHERE pos = 1
     ORDER BY hashtag, HashTagCount DESC
     """
-    json_result = execute_query(sql)
-    print json.loads(json_result)
+    query_string = build_q2_querystring()
+    #print "QUERY STRING: ",query_string
+    json_result = execute_query(query_string)
+    #parsed_results = json.loads(json_result)
+    #final_result = map_q2_results_to_language(parsed_results)
+    #print "QUERY RESULT: ",query_result
     return json_result
 
 if __name__ == '__main__':
