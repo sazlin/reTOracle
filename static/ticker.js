966acdb299eb626f8d3e978e1f4c8040f2a88105
John Shiver <johneshiver@gmail.com>
2014-06-26 17:53:45 -0700

    Updated CSS and div elements

--- static/ticker.js
+++ static/ticker.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
         $.get("/ticker", '', function(data) {
             var val1 = data[0][0];
             var val2 = data[0][1];
-            $(".twitter_tick").replaceWith("<p>" + val1 + ": " + val2 + "</p>");
+            $(".twitter_tick p").replaceWith("<p><b>" + val1 + "</b>" + ": " + val2 + "</p>");
+            $(".twitter_tick p").css("font-size: 15px");
          }, "json" );
     }, 3000)
 });
