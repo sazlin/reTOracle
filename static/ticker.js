a4ba9a02f7a9987b692d4946c61a74e06ca39662
John Shiver <johneshiver@gmail.com>
2014-06-26 17:22:10 -0700

    updated jQuery, ticker now technically works

--- static/ticker.js
+++ static/ticker.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     setInterval(function(){
-        $.get("/ticker", function(data) {
-            var data = $.parseJSON(data)
-            $("#twitter_tick").append(data);
+        $.get("/ticker", '', function(data) {
+            var val1 = data[0][0];
+            var val2 = data[0][1];
+            $(".twitter_tick").replaceWith("<p>" + val1 + ": " + val2 + "</p>");
          }, "json" );
     }, 3000)
 });
