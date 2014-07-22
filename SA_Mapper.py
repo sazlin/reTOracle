d0f32c47d8efb0244829f2aa9fd7bf0bede60171
Sean Azlin <sazlin@gmail.com>
2014-07-21 17:36:49 -0700

    Updated SA_Mapper.py to now continuously grab batches of tweets and fake-process them. It also restricts itself to one batch per 5 seconds, which eventually will be important when there aren't that many tweets per batch.

--- SA_Mapper.py
+++ SA_Mapper.py
@@ -6,33 +6,30 @@
 # import sys
 import json
 import sql_queries as sql_q
+import time
 
+MIN_EXECUTION_PERIOD = 5.0
 
 def main(separator='\t'):
     #get batch of tweet
     sql_q.init()
-    json_results = sql_q.get_query_results('num_tweets_need_sa')
-    print "Json results:", json_results
-    num_todo = json.loads(json_results)
-    print "Num Todo:", num_todo
-    tweets_to_analyze = json.loads(sql_q.get_query_results('tweet_batch'))
-    #print tweets_to_analyze
-    for tweet in tweets_to_analyze:
-        # import pdb; pdb.set_trace()
-        sql_q.get_query_results(
-            'set_tweet_sent',
-            (tweet[0], 1),
-            False)
-        print tweet[0], 1
-
-    # data = read_input(sys.stdin)
-    # d = {'test_val': 1}
-    #do sentiment analysis on tweet here
-    # write the results to STDOUT (standard output);
-    # what we output here will be the input for the
-    # Reduce step, i.e. the input for reducer.py
-    #
-     # print json.dumps(d)
+    while True:
+        json_results = sql_q.get_query_results('num_tweets_need_sa')
+        num_todo = json.loads(json_results)
+        last_check = time.time()
+        tweet_batch = json.loads(sql_q.get_query_results('tweet_batch'))
+        for tweet in tweet_batch:
+            # import pdb; pdb.set_trace()
+            sql_q.get_query_results(
+                'set_tweet_sent',
+                (tweet[0], 1),
+                False)
+            print tweet[0], 1
+        time_spent = time.time() - last_check
+        if time_spent < MIN_EXECUTION_PERIOD:
+            print "Too fast... sleeping..."
+            time.sleep(MIN_EXECUTION_PERIOD - time_spent)
+            print "Nap over! Back to work."
 
 if __name__ == "__main__":
     main()
