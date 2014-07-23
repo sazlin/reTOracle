b013b6bd91b332337223ceeb9c94d14968a764f4
John Shiver <johneshiver@gmail.com>
2014-07-23 15:26:01 -0700

    Changed predict function to take classifier and vetorizer arguments / will greatly improve runtime

--- Sentiment Analysis/NB.py
+++ Sentiment Analysis/NB.py
@@ -1,18 +1,8 @@
 from sklearn.datasets import load_files
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from pickleMe import load_pickle, export_pickle
 
-
-# train_small = load_files('./training_data/')
-# test_small = load_files('./test_data/')
-
-# Turn the text documents into vectors of word frequencies
-# vectorizer = CountVectorizer(min_df=5, ngram_range=(1, 2),
-#                              stop_words='english',
-#                              strip_accents='ascii')
-# X_train = vectorizer.fit_transform(train_small.data)
-# y_train = train_small.target
 
 
 def export_classifier():
@@ -40,10 +30,12 @@ def export_classifier():
     export_pickle('vectorizer.txt', vectorizer)
 
 
-def predict(tweet):
+classifier_global = load_pickle('classifier.txt')
+vectorizer_global = load_pickle('vectorizer.txt')
 
-    classifier = load_pickle('classifier.txt')
-    vectorizer = load_pickle('vectorizer.txt')
+
+def predict(tweet, classifier=classifier_global, vectorizer=vectorizer_global):
+
     tweet = [tweet]
     matrix2 = vectorizer.transform(tweet).todense()
     predicted = classifier.predict(matrix2)
