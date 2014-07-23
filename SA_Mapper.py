9983acaa54c99063b87783240097321d9022735e
Sean Azlin <sazlin@gmail.com>
2014-07-22 18:52:11 -0700

    Fixed SA_Mapper.py so it works on EMR.

--- SA_Mapper.py
+++ SA_Mapper.py
@@ -5,20 +5,21 @@ import sys
 
 
 def main(argv):
-    try:
-        print "reading input..."
-        json_result = sys.stdin.read()
-        print json_result
-        print "parsing json...",
+    # try:
+    for line in sys.stdin:
+        # try:
         try:
-            tweet_batch = json.loads(json_result)
-        except Exception as x:
-            print "Error parsing json: ", x.args
-        print "Done."
-        for tweet in tweet_batch:
-            print tweet[0] + '\t' + '1'
-    except Exception as x:
-        return "Something went wrong: ", x.args
+            tweet = json.loads(line)
+        except Exception:
+            pass  # skip this tweet
+        else:
+            # except Exception as x:
+            #     print "Error parsing json: ", x.args
+            # print "Done."
+            # for tweet in tweet_batch:
+            print str(tweet[0]) + '\t' + '1'
+    # except Exception as x:
+    #     return "Something went wrong: ", x.args
 
 if __name__ == "__main__":
     main(sys.argv)
