4d3bfccfa583018ee0836cff1f3f84d2e9cccbe2
Sean Azlin <sazlin@gmail.com>
2014-07-23 13:22:27 -0700

    Added scaffolding for a set of unit tests for SA_Worker.py. A lot more work is needed to setup a proper test environment for the tests since every method needs access to one or more databases or other cloud resources.

---
+++ test_sa_worker.py
@@ -0,0 +1,45 @@
+#import pytest
+#import SA_Worker.py
+
+
+# We need to think through and create robust fixtures that create
+# proper test environments for unit/component testing before we
+# can have functioning test
+def test_delete_key_if_exists():
+    pass
+
+
+def test_cleanup():
+    pass
+
+
+def test_create_sa_input():
+    pass
+
+
+def test_copy_sa_mapper():
+    pass
+
+
+def test_copy_sa_reducer():
+    pass
+
+
+def test_create_inputs():
+    pass
+
+
+def test_get_num_tweets_need_sa():
+    pass
+
+
+def test_create_sa_job():
+    pass
+
+
+def test_wait_for_job_to_complete():
+    pass
+
+
+def test_push_sa_results_to_sql_from_s3():
+    pass
