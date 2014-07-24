bbd53ec82fbadd56a63045863c54e97d8aceb55e
Sean Azlin <sazlin@gmail.com>
2014-07-24 15:16:26 -0700

    Updated the sa_reducer.py script to aggregate the sentiment scores from all SA methods and add those to the output.

--- sa_reducer.py
+++ sa_reducer.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python
 import sys
+import json
+from SentimentAnalysis import agg_sent
 #identity reducer
 for line in sys.stdin:
-    # line = line.strip()
-    # arg = line.split('\t')
-    # print "[{0}, {1}]".format(arg[0], arg[1])
     if line == "\n":
         pass
     else:
-        print line
+        try:
+            result_dict = json.loads(line)
+        except Exception:
+            #invalid input, so skip this line
+            pass
+        else:
+            neg_probs = []
+            pos_probs = []
+            for key in result_dict:
+                if "_neg_" in key.lower():
+                    neg_probs.append(result_dict[key])
+                elif "_pos_" in key.lower():
+                    pos_probs.append(result_dict[key])
+            agg_sent_result = agg_sent.get_agg_sent(neg_probs, pos_probs)
+            result_dict['agg_sent'] = agg_sent_result[0]
+            result_dict['agg_prob'] = agg_sent_result[1]
+            print json.dumps(result_dict)
