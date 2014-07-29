5b95080459e6c50cd3dbbdfe869c02598fc153bd
miramzz <mira.muazzez@gmail.com>
2014-07-29 09:10:14 -0700

    starting to write tcs

--- test_rheTOracle.py
+++ test_rheTOracle.py
@@ -1,3 +1,27 @@
+import pytest
+import rheTOracle
+from flask.ext.testing import
+
 def test_say_hello():
     from rheTOracle import say_hello
     assert say_hello() == u'RheTOracly speaking, how awesome are we?'
+
+
+def test_update_redis():
+    pass
+
+def test_q1_query():
+    pass
+
+
+def test_q2_query():
+    pass
+
+
+def test_map_q2_results_to_language():
+    pass
+
+
+def test_map_q1_results_to_language():
+    pass
+
